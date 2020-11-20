13f50b0f29d0802dd7d6bb18a37270b8930ad57d
nastassia-batashova <batashofa@gmail.com>
2020-11-20 16:59:30 +0300

    .header-navbar__btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,5 @@
-
-$(document).ready(function () {
-
-  $('.header-navbar__hamburger').on('click', function () {
-    $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
-  });
-
+$(document).ready(function(){
+	$('.header-navbar__btn').click(function(){
+		$(this).toggleClass('open');
+	});
 });
